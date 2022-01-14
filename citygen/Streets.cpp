85fd7ae0ea593d77def562161aa4a39f8565a8fd
zz <xzrunner@gmail.com>
2022-01-14 14:11:18 +0800

    [FIXED] PathCutBy()

--- citygen/Streets.cpp
+++ citygen/Streets.cpp
@@ -280,12 +280,10 @@ std::vector<sm::vec2> Streets::PathCutBy(const std::vector<sm::vec2>& base, cons
 		{
 			if (sm::is_point_in_segment(cut[j], base[i], base[i + 1])) 
 			{
-				float dist = sm::dis_pos_to_pos(cut[j], base[i]);
+				float d = sm::dis_pos_to_pos(cut[j], base[i]) / sm::dis_pos_to_pos(base[i], base[i + 1]);
+				sm::vec2 new_p = base[i] + (base[i + 1] - base[i]) * d;
 
-				float tot = sm::dis_pos_to_pos(base[i], base[i + 1]);
-				sm::vec2 new_p = base[i] + (base[i + 1] - base[i]) / tot * dist;
-
-				new_pts.insert({ i + dist, new_p });
+				new_pts.insert({ i + d, new_p });
 			}
 		}
 	}
@@ -298,15 +296,26 @@ std::vector<sm::vec2> Streets::PathCutBy(const std::vector<sm::vec2>& base, cons
 			sm::vec2 new_p;
 			if (sm::intersect_segment_segment(base[i], base[i + 1], cut[j], cut[j + 1], &new_p)) 
 			{
-				float d = sm::dis_pos_to_pos(new_p, base[i]) / sm::dis_pos_to_pos(base[i], base[i + 1]);
-				new_pts.insert({ i + d, new_p });
+				bool exist = false;
+				for (auto& np : new_pts) {
+					if (sm::dis_pos_to_pos(np.second, new_p) < SM_LARGE_EPSILON) {
+						exist = true;
+						break;
+					}
+				}
+
+				if (!exist) {
+					float d = sm::dis_pos_to_pos(new_p, base[i]) / sm::dis_pos_to_pos(base[i], base[i + 1]);
+					new_pts.insert({ i + d, new_p });
+				}
 			}
 		}
 	}
 
+	std::vector<sm::vec2> ret;
+
 	auto itr = new_pts.begin();
 
-	std::vector<sm::vec2> ret;
 	for (size_t i = 0, n = base.size(); i < n; ++i)
 	{
 		ret.push_back(base[i]);
