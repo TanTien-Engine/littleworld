a4fd8ed68ab8be23732a873d8f5e1b006eb8c936
zz <xzrunner@gmail.com>
2022-02-12 15:03:40 +0800

    [ADDED] draw lines and polyline with line_strip and prim_restart

--- globegen/ShapeBatching.cpp
+++ globegen/ShapeBatching.cpp
@@ -4,6 +4,8 @@
 #include <geoshape/Line3D.h>
 #include <geoshape/Polyline3D.h>
 
+#define PRIMITIVE_RESTART
+
 namespace globegen
 {
 
@@ -12,7 +14,11 @@ void ShapeBatching::Add(const std::shared_ptr<gs::Shape3D>& shape, uint32_t col)
 	auto line = std::dynamic_pointer_cast<gs::Line3D>(shape);
 	if (line)
 	{
+#ifdef PRIMITIVE_RESTART
+		m_buf.Reserve(3, 2);
+#else
 		m_buf.Reserve(2, 2);
+#endif // PRIMITIVE_RESTART
 
 		m_buf.vert_ptr[0].pos = line->GetStart();
 		m_buf.vert_ptr[0].col = col;
@@ -22,7 +28,12 @@ void ShapeBatching::Add(const std::shared_ptr<gs::Shape3D>& shape, uint32_t col)
 
 		m_buf.index_ptr[0] = m_buf.curr_index;
 		m_buf.index_ptr[1] = m_buf.curr_index + 1;
+#ifdef PRIMITIVE_RESTART
+		m_buf.index_ptr[2] = 0xffff;
+		m_buf.index_ptr += 3;
+#else
 		m_buf.index_ptr += 2;
+#endif // PRIMITIVE_RESTART
 		m_buf.curr_index += 2;
 
 		return;
@@ -34,7 +45,17 @@ void ShapeBatching::Add(const std::shared_ptr<gs::Shape3D>& shape, uint32_t col)
 		auto& pts = polyline->GetVertices();
 		unsigned short num = static_cast<unsigned short>(pts.size());
 
-		m_buf.Reserve(num, num);
+		auto is_closed = polyline->GetClosed();
+
+		size_t idx_num = num;
+#ifdef PRIMITIVE_RESTART
+		idx_num++;
+#endif // PRIMITIVE_RESTART
+		if (is_closed) {
+			idx_num++;
+		}
+
+		m_buf.Reserve(idx_num, num);
 
 		for (size_t i = 0; i < num; ++i) {
 			m_buf.vert_ptr[i].pos = pts[i];
@@ -45,7 +66,13 @@ void ShapeBatching::Add(const std::shared_ptr<gs::Shape3D>& shape, uint32_t col)
 		for (unsigned short i = 0; i < num; ++i) {
 			m_buf.index_ptr[i] = m_buf.curr_index + i;
 		}
-		m_buf.index_ptr += num;
+		if (is_closed) {
+			m_buf.index_ptr[num] = m_buf.index_ptr[0];
+		}
+#ifdef PRIMITIVE_RESTART
+		m_buf.index_ptr[idx_num - 1] = 0xffff;
+#endif // PRIMITIVE_RESTART
+		m_buf.index_ptr += idx_num;
 		m_buf.curr_index += num;
 	}
 }
