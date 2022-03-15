a17a6fd7f1a82069e7e9282faba61b590776a8ff
zz <xzrunner@gmail.com>
2022-03-15 17:22:26 +0800

    [ADDED] ocean with atmo, skybox and horizon view

--- globegen/wrap_GlobeGen.cpp
+++ globegen/wrap_GlobeGen.cpp
@@ -384,6 +384,131 @@ void w_GlobeTools_cam_position()
     tt::return_list(std::vector<float>{ p.x, p.y, p.z });
 }
 
+void w_GlobeTools_hori_cam_view_mat()
+{
+    float h     = ves_tonumber(1);
+    float theta = ves_tonumber(2);
+    float phi   = ves_tonumber(3);
+
+    float m[16] = {
+         0.0, 0.0, -1.0, 0.0,
+        -1.0, 0.0,  0.0, 0.0,
+         0.0, 1.0,  0.0, 0.0,
+         0.0,  -h,  0.0, 1.0
+    };
+    sm::mat4 view;
+    memcpy(view.c, m, sizeof(m));
+
+    view = sm::mat4::RotatedY(SM_RAD_TO_DEG * phi) * view;
+    view = sm::mat4::RotatedX(SM_RAD_TO_DEG * theta) * view;
+
+    ves_pop(ves_argnum());
+
+    ves_pushnil();
+    ves_import_class("maths", "Matrix44");
+    sm::mat4* mat = (sm::mat4*)ves_set_newforeign(0, 1, sizeof(sm::mat4));
+    *mat = view;
+    ves_pop(1);
+}
+
+void w_GlobeTools_hori_cam_proj_mat()
+{
+    float h = ves_tonumber(1);
+
+    float width  = tt::Graphics::Instance()->GetWidth();
+    float height = tt::Graphics::Instance()->GetHeight();
+
+    sm::mat4 proj = sm::mat4::Perspective(90.0f, float(width) / float(height), 0.1f * h, 1000000.0f * h);
+
+    ves_pop(ves_argnum());
+
+    ves_pushnil();
+    ves_import_class("maths", "Matrix44");
+    sm::mat4* mat = (sm::mat4*)ves_set_newforeign(0, 1, sizeof(sm::mat4));
+    *mat = proj;
+    ves_pop(1);
+}
+
+void w_GlobeTools_gen_ocean_mesh()
+{
+    float grid_size = ves_tonumber(1);
+    float cam_theta = ves_tonumber(2);
+
+    float width = tt::Graphics::Instance()->GetWidth();
+    float height = tt::Graphics::Instance()->GetHeight();
+
+    float horizon = tan(cam_theta / 180.0 * SM_PI);
+    float s = std::min(1.1f, 0.5f + horizon * 0.5f);
+
+    float vmargin = 0.1;
+    float hmargin = 0.1;
+
+    sm::vec4* vertices = new sm::vec4[int(ceil(height * (s + vmargin) / grid_size) + 5) * int(ceil(width * (1.0 + 2.0 * hmargin) / grid_size) + 5)];
+
+    int vert_num = 0;
+    int nx = 0;
+    for (float j = height * s - 0.1; j > -height * vmargin - grid_size; j -= grid_size) {
+        nx = 0;
+        for (float i = -width * hmargin; i < width * (1.0 + hmargin) + grid_size; i += grid_size) {
+            vertices[vert_num++] = sm::vec4(-1.0 + 2.0 * i / width, -1.0 + 2.0 * j / height, 0.0, 1.0);
+            nx++;
+        }
+    }
+
+    int idx_num = 0;
+    unsigned int* indices = new unsigned int[6 * int(ceil(height * (s + vmargin) / grid_size) + 4) * int(ceil(width * (1.0 + 2.0 * hmargin) / grid_size) + 4)];
+
+    int nj = 0;
+    for (float j = height * s - 0.1; j > -height * vmargin; j -= grid_size) {
+        int ni = 0;
+        for (float i = -width * hmargin; i < width * (1.0 + hmargin); i += grid_size) {
+            indices[idx_num++] = ni + (nj + 1) * nx;
+            indices[idx_num++] = (ni + 1) + (nj + 1) * nx;
+            indices[idx_num++] = (ni + 1) + nj * nx;
+            indices[idx_num++] = (ni + 1) + nj * nx;
+            indices[idx_num++] = ni + (nj + 1) * nx;
+            indices[idx_num++] = ni + nj * nx;
+            ni++;
+        }
+        nj++;
+    }
+
+    auto dev = tt::Render::Instance()->Device();
+
+    auto va = dev->CreateVertexArray();
+
+    auto usage = ur::BufferUsageHint::StaticDraw;
+
+    auto ibuf = dev->CreateIndexBuffer(usage, 0);
+    int ibuf_sz = static_cast<int>(sizeof(unsigned int) * idx_num);
+    ibuf->SetCount(static_cast<int>(idx_num));
+    ibuf->Reserve(ibuf_sz);
+    ibuf->ReadFromMemory(indices, ibuf_sz, 0);
+    ibuf->SetDataType(ur::IndexBufferDataType::UnsignedInt);
+    va->SetIndexBuffer(ibuf);
+
+    auto vbuf = dev->CreateVertexBuffer(ur::BufferUsageHint::StaticDraw, 0);
+    int vbuf_sz = static_cast<int>(sizeof(sm::vec4) * vert_num);
+    vbuf->Reserve(vbuf_sz);
+    vbuf->ReadFromMemory(vertices, vbuf_sz, 0);
+    va->SetVertexBuffer(vbuf);
+    va->SetVertexBufferAttrs({
+        std::make_shared<ur::VertexInputAttribute>(0, ur::ComponentDataType::Float,        3,  0, 16),		// position
+		std::make_shared<ur::VertexInputAttribute>(1, ur::ComponentDataType::UnsignedByte, 4, 12, 16)		// color
+    });
+
+    delete[] vertices;
+    delete[] indices;
+
+    ves_pop(ves_argnum());
+
+    ves_pushnil();
+    ves_import_class("render", "VertexArray");
+    auto proxy = (tt::Proxy<ur::VertexArray>*)ves_set_newforeign(0, 1, sizeof(tt::Proxy<ur::VertexArray>));
+    proxy->obj = va;
+    ves_pop(1);
+}
+
 const unsigned char g_bit_rev_table[] = {
     0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0, 0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8, 0x04, 0x84, 0x44, 0xC4, 0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4, 0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC, 0x3C, 0xBC, 0x7C, 0xFC, 0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2, 0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2, 0x72, 0xF2, 0x0A, 0x8A, 0x4A, 0xCA, 0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA, 0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96, 0x56, 0xD6, 0x36, 0xB6, 0x76, 0xF6, 0x0E, 0x8E, 0x4E, 0xCE, 0x2E, 0xAE, 0x6E, 0xEE, 0x1E, 0x9E, 0x5E, 0xDE, 0x3E, 0xBE, 0x7E, 0xFE, 0x01, 0x81, 0x41, 0xC1, 0x21, 0xA1, 0x61, 0xE1, 0x11, 0x91, 0x51, 0xD1, 0x31, 0xB1, 0x71, 0xF1, 0x09, 0x89, 0x49, 0xC9, 0x29, 0xA9, 0x69, 0xE9, 0x19, 0x99, 0x59, 0xD9, 0x39, 0xB9, 0x79, 0xF9, 0x05, 0x85, 0x45, 0xC5, 0x25, 0xA5, 0x65, 0xE5, 0x15, 0x95, 0x55, 0xD5, 0x35, 0xB5, 0x75, 0xF5, 0x0D, 0x8D, 0x4D, 0xCD, 0x2D, 0xAD, 0x6D, 0xED, 0x1D, 0x9D, 0x5D, 0xDD, 0x3D, 0xBD, 0x7D, 0xFD, 0x03, 0x83, 0x43, 0xC3, 0x23, 0xA3, 0x63, 0xE3, 0x13, 0x93, 0x53, 0xD3, 0x33, 0xB3, 0x73, 0xF3, 0x0B, 0x8B, 0x4B, 0xCB, 0x2B, 0xAB, 0x6B, 0xEB, 0x1B, 0x9B, 0x5B, 0xDB, 0x3B, 0xBB, 0x7B, 0xFB, 0x07, 0x87, 0x47, 0xC7, 0x27, 0xA7, 0x67, 0xE7, 0x17, 0x97, 0x57, 0xD7, 0x37, 0xB7, 0x77, 0xF7, 0x0F, 0x8F, 0x4F, 0xCF, 0x2F, 0xAF, 0x6F, 0xEF, 0x1F, 0x9F, 0x5F, 0xDF, 0x3F, 0xBF, 0x7F, 0xFF
 };
@@ -462,7 +587,9 @@ VesselForeignMethodFn GlobeGenBindMethod(const char* signature)
     if (strcmp(signature, "static GlobeTools.cam_view_mat(_,_,_,_,_)") == 0) return w_GlobeTools_cam_view_mat;
     if (strcmp(signature, "static GlobeTools.cam_proj_mat(_,_,_,_,_)") == 0) return w_GlobeTools_cam_proj_mat;
     if (strcmp(signature, "static GlobeTools.cam_position(_,_,_,_,_)") == 0) return w_GlobeTools_cam_position;
-
+    if (strcmp(signature, "static GlobeTools.hori_cam_view_mat(_,_,_)") == 0) return w_GlobeTools_hori_cam_view_mat;
+    if (strcmp(signature, "static GlobeTools.hori_cam_proj_mat(_)") == 0) return w_GlobeTools_hori_cam_proj_mat;
+    if (strcmp(signature, "static GlobeTools.gen_ocean_mesh(_,_)") == 0) return w_GlobeTools_gen_ocean_mesh;
     if (strcmp(signature, "static GlobeTools.generate_bit_reversed_indices(_)") == 0) return w_GlobeTools_generate_bit_reversed_indices;
 
     return nullptr;
